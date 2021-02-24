b169400e94cf476e0e80135f981e8f64c10c8748
Nikolic-Marko <nikolicmarko653@gmail.com>
2021-02-24 19:22:15 +0100

    CleanUp

--- src/component/TimeLine/timelineElements.js
+++ src/component/TimeLine/timelineElements.js
@@ -1,7 +1,7 @@
 let timelineElements = [
   {
     id: 1,
-    startDate: "25.1.2021",
+    startDate: "25.1.2021.",
     title: "Otvaranje prijava",
     description:
       "Okupite tim, sastavite prijavu i popunite formu na sajtu!",
@@ -9,7 +9,7 @@ let timelineElements = [
   },
   {
     id: 2,
-    startDate: "11.3.2021",
+    startDate: "11.3.2021.",
     title: "Zatvaranje prijava",
     description:
       "Nakon zatvaranja prijava, prijavljeni timovi će biti kontaktirani i biće im dostavljene sve neophodne informacije vezane za dalji tok takmičenja.",
@@ -17,7 +17,7 @@ let timelineElements = [
   },
   {
     id: 3,
-    startDate: "14.3.2021",
+    startDate: "14.3.2021.",
     title: "Online test",
     description:
       "Svaki tim radi test, nakon čega se u najkraćem mogućem roku dostavljaju rezultati testa kao i informacija o prolasku u naredni krug selekcije.",
@@ -25,7 +25,7 @@ let timelineElements = [
   },
   {
     id: 4,
-    startDate: "18.3.2021 - 21.3.2021",
+    startDate: "18.3.2021. - 21.3.2021.",
     title: "Radionice",
     description:
       "Podrazumevaju upoznavanje potencijalnih učesnika sa tehnologijama koje će im biti od pomoći na samom hakatonu. Nakon odslušanih radionica, timovima se zadaje projektni zadatak koji je neophodno da odrade i predaju na vreme, i koji ujedno predstavlja drugi krug selekcije.",
@@ -33,7 +33,7 @@ let timelineElements = [
   },
   {
     id: 5,
-    startDate: "2.4.-3.4.",
+    startDate: "2.4.2021. - 3.4.2021.",
     title: " Hakaton",
     description:
       "Timovima se predstavlja tema hakatona, nakon čega imaju na raspolaganju 24 sata da osmisle što bolje i kreativnije rešenje, i na taj način osvoje prvo mesto!",
