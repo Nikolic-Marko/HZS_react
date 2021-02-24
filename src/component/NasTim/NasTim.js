df818e1276b943b41641513e94eb695ae4580ca3
StefanFele <stefivukovic@gmail.com>
2021-02-24 13:29:59 +0100

    tim responzivnost

--- src/component/NasTim/NasTim.js
+++ src/component/NasTim/NasTim.js
@@ -10,15 +10,15 @@ const NasTim = () => {
   const responsive = {
     superLargeDesktop: {
       // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5
-    },
-    desktop: {
       breakpoint: { max: 3000, min: 1024 },
       items: 4
     },
+    desktop: {
+      breakpoint: { max: 1024, min: 800 },
+      items: 3
+    },
     tablet: {
-      breakpoint: { max: 1024, min: 700 },
+      breakpoint: { max: 800, min: 700 },
       items: 2
     },
     mobile: {
