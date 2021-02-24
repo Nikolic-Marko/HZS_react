eedd3c4a27535180e880547d4325f42f5ace3286
bane <bane.stojanovic00@gmail.com>
2021-02-24 18:22:02 +0100

    zavrsena prijava

--- src/App.js
+++ src/App.js
@@ -10,15 +10,15 @@ import Prijava from './component/Prijava/Prijava'
 function App() {
 
   return (
-      <Switch>
-        <Route path="/" component={Homepage} />
-        <Route path="/forum" exact component={Forum} />
-        <Route path="/forum/pitanja" exact component={ForumPitanja} />
-        <Route path="/forum/takmicenja" exact component={ForumPitanja} />
-        <Route path="/forum/ostalo" exact  component={ForumPitanja} />
-        <Route path="/forum/ostalo/:id" exact component={PitanjaStranica} />
-        <Route path="/prijava" exact component={Prijava} />
-      </Switch>
+    <Switch>
+      <Route path="/" exact component={Homepage} />
+      <Route path="/forum" exact component={Forum} />
+      <Route path="/forum/pitanja" exact component={ForumPitanja} />
+      <Route path="/forum/takmicenja" exact component={ForumPitanja} />
+      <Route path="/forum/ostalo" exact component={ForumPitanja} />
+      <Route path="/forum/ostalo/:id" exact component={PitanjaStranica} />
+      <Route path="/prijava" exact component={Prijava} />
+    </Switch>
 
   )
 }
